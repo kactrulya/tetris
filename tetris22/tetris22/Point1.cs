bc5271acdb4ccbec1771f7cea11fcc6e0d0f68a3
Nikita Pushkarev <greenfrost5@yandex.ru>
2024-05-18 15:02:17 +0300

    dobavil tochki

---
+++ tetris22/tetris22/Point1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris22
+{
+    internal class Point1
+    {
+        public int x;
+        public int y;
+        public char c;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(c);
+        }
+    }
+}
